5b47be3d12d6f03634baf7f962f9af89293d10aa
Cory Huckeba <coryhuckeba@Corys-MacBook-Pro.local>
2018-09-04 09:57:17 -0400

    Added Firebase initialization

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+
 import { Header } from './components/common';
-// import Header from './components/common/Header';
 
 class App extends Component {
-    render() {
-        return (
-            <View>
-                <Header headerText="Authentication" />
-                <Text>An app</Text>
-            </View>
-        );
-    }
+  componentWillMount() {    
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDko7wM-OnCzYQ6tgp06Zi5nxEbPEhIgQU',
+      authDomain: 'react-native-auth-64a63.firebaseapp.com',
+      databaseURL: 'https://react-native-auth-64a63.firebaseio.com',
+      projectId: 'react-native-auth-64a63',
+      storageBucket: 'react-native-auth-64a63.appspot.com',
+      messagingSenderId: '832784902294'
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        <Text>An app</Text>
+      </View>
+    );
+  }
 }
 
 export default App;
